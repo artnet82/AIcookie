938b9c5ed5975a2f19de80de2ebb189ead46f745
artnet82 <108236913+artnet82@users.noreply.github.com>
2023-12-11 20:01:34 +0300

    Update app.py
    Внёс кнопки удаления модели ) ну и функции..

--- app.py
+++ app.py
@@ -1,76 +1,82 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import io
+import seaborn as sns
 import base64
+import io
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+# Генерация предсказания на основе данных пользователя
+def generate_prediction(user_data, accuracy_setting):
+    # Ваш код для генерации предсказания на основе данных пользователя
+    # Пример: просто возвращаем строку "Мужчина, Возраст: 30"
+    prediction = f"Мужчина, Возраст: {accuracy_setting}"
+    return prediction
+
+# Генерация графика тепловой карты на основе предсказания
+def generate_heatmap_plot(prediction):
+    # Ваш код для генерации графика тепловой карты
+    # Пример: создаем фиктивный DataFrame и строим тепловую карту
+    data = {'Мужчина': [0.8, 0.2], 'Женщина': [0.2, 0.8]}
+    df = pd.DataFrame(data, index=['Молодой', 'Пожилой'])
+    sns.heatmap(df, annot=True, cmap='YlGnBu')
+    plot = sns.plt.gcf()
+    return plot
+
+# Расчет точности предсказаний
+def calculate_accuracy(prediction):
+    # Ваш код для расчета точности предсказаний
+    # Пример: просто возвращаем значение 0.85 в качестве точности
+    accuracy = 0.85
+    return accuracy
+
+# Проверка наличия модели
+def check_model():
+    return os.path.exists('models/model.pkl')
+
+# Удаление модели
+def delete_model():
+    os.remove('models/model.pkl')
+
+@app.route('/', methods=['GET'])
+def index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file.filename.endswith('.csv'):
-            # Чтение данных из загруженного файла
-            data = pd.read_csv(file)
-            
-            # Подготовка признаков и меток
-            X = data["internet_activity"]
-            y_gender = data["gender"]
-            y_age = data["age"]
+    # Получение загруженного файла
+    file = request.files['file']
+    # Загрузка и обработка данных из файла
+    df = pd.read_csv(file)
+    user_data = df.values.tolist()
+    # Получение выбранного уровня точности
+    accuracy_setting = request.form['accuracy']
+    # Генерация предсказания на основе данных файла и выбранного уровня точности
+    prediction = generate_prediction(user_data, accuracy_setting)
+    # Генерация графика тепловой карты
+    plot = generate_heatmap_plot(prediction)
+    # Конвертация графика в изображение в формате base64
+    buffer = io.BytesIO()
+    plot.savefig(buffer, format='png')
+    buffer.seek(0)
+    plot_image = base64.b64encode(buffer.getvalue()).decode()
+    # Расчет точности предсказаний
+    accuracy = calculate_accuracy(prediction)
+    # Отображение результатов
+    return render_template('results.html', accuracy=accuracy, plot_url=plot_image)
 
-            # Разделение данных на обучающую и тестовую выборки
-            X_train, X_test, y_gender_train, y_gender_test, y_age_train, y_age_test = train_test_split(X, y_gender, y_age, test_size=0.2, random_state=42)
+@app.route('/download-model', methods=['GET'])
+def download_model():
+    return send_file('models/model.pkl', as_attachment=True, attachment_filename='model.pkl')
 
-            # Преобразование текстовых признаков в числовые с помощью мешка слов
-            vectorizer = CountVectorizer()
-            X_train_vectorized = vectorizer.fit_transform(X_train)
-            X_test_vectorized = vectorizer.transform(X_test)
-
-            # Обучение модели для определения пола с настройкой гиперпараметров
-            gender_classifier = LogisticRegression()
-            gender_param_grid = {'C': [0.1, 1, 10]}
-            gender_grid_search = GridSearchCV(gender_classifier, gender_param_grid)
-            gender_grid_search.fit(X_train_vectorized, y_gender_train)
-            gender_best_model = gender_grid_search.best_estimator_
-
-            # Предсказание пола для тестовых данных
-            gender_predictions = gender_best_model.predict(X_test_vectorized)
-
-            # Оценка точности предсказаний пола
-            gender_accuracy = accuracy_score(y_gender_test, gender_predictions)
-
-            # Визуализация результатов предсказания пола
-            gender_labels = np.unique(y_gender_test)
-            gender_cm = confusion_matrix(y_gender_test, gender_predictions, labels=gender_labels)
-            gender_cm_df = pd.DataFrame(gender_cm, index=gender_labels, columns=gender_labels)
-
-            # Генерация графика тепловой карты
-            plt.figure(figsize=(8, 6))
-            plt.title('Confusion Matrix - Gender Prediction')
-            sns.heatmap(gender_cm_df, annot=True, cmap='Blues')
-            plt.xlabel('Predicted')
-            plt.ylabel('True')
-
-            # Преобразование графика в изображение в формате base64
-            img = io.BytesIO()
-            plt.savefig(img, format='png', bbox_inches='tight')
-            img.seek(0)
-            plot_url = base64.b64encode(img.getvalue()).decode()
-
-            # Отправка результатов на страницу
-            return render_template('results.html', accuracy=gender_accuracy, plot_url=plot_url)
-
-    return "Invalid file format. Please upload a CSV file."
+@app.route('/reset-model', methods=['GET'])
+def reset_model():
+    if check_model():
+        delete_model()
+        return "Модель успешно сброшена и удалена."
+    else:
+        return "Модель уже отсутствует."
 
 if __name__ == '__main__':
     app.run()
